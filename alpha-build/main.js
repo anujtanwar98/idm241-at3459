6046e7e581808d74be888492ffb97c5f725704eb
Anuj <anujtanwar89@gmail.com>
2022-10-14 00:18:45 -0400

    updates

--- alpha-build/main.js
+++ alpha-build/main.js
@@ -29,42 +29,44 @@
 //     supportMenu.classList.toggle("show");
 // });
 
-const shippingTitle = document.querySelector(".js-shipping-link");
-const shippingMenu = document.querySelector(".js-shipping");
-const trackingTitle = document.querySelector(".js-tracking-link");
-const trackingMenu = document.querySelector(".js-tracking");
-const businessTitle = document.querySelector(".js-business-link");
-const businessMenu = document.querySelector(".js-business");
-const supportTitle = document.querySelector(".js-support-link");
-const supportMenu = document.querySelector(".js-support");
+//  for reference in future versions
+
+// const shippingTitle = document.querySelector(".js-shipping-link");
+// const shippingMenu = document.querySelector(".js-shipping");
+// const trackingTitle = document.querySelector(".js-tracking-link");
+// const trackingMenu = document.querySelector(".js-tracking");
+// const businessTitle = document.querySelector(".js-business-link");
+// const businessMenu = document.querySelector(".js-business");
+// const supportTitle = document.querySelector(".js-support-link");
+// const supportMenu = document.querySelector(".js-support");
 
 
-shippingTitle.addEventListener('click', () => {
-    shippingMenu.classList.toggle("show");
-    trackingMenu.classList.remove("show");
-    businessMenu.classList.remove("show");
-    supportMenu.classList.remove("show");
-});
+// shippingTitle.addEventListener('click', () => {
+//     shippingMenu.classList.toggle("show");
+//     trackingMenu.classList.remove("show");
+//     businessMenu.classList.remove("show");
+//     supportMenu.classList.remove("show");
+// });
 
-trackingTitle.addEventListener('click', () => {
-    trackingMenu.classList.toggle("show");
-    shippingMenu.classList.remove("show");
-    businessMenu.classList.remove("show");
-    supportMenu.classList.remove("show");
-});
+// trackingTitle.addEventListener('click', () => {
+//     trackingMenu.classList.toggle("show");
+//     shippingMenu.classList.remove("show");
+//     businessMenu.classList.remove("show");
+//     supportMenu.classList.remove("show");
+// });
 
-businessTitle.addEventListener('click', () => {
-    businessMenu.classList.toggle("show");
-    supportMenu.classList.remove("show");
-    shippingMenu.classList.remove("show");
-    trackingMenu.classList.remove("show");
-});
+// businessTitle.addEventListener('click', () => {
+//     businessMenu.classList.toggle("show");
+//     supportMenu.classList.remove("show");
+//     shippingMenu.classList.remove("show");
+//     trackingMenu.classList.remove("show");
+// });
 
-supportTitle.addEventListener('click', () => {
-    supportMenu.classList.toggle("show");
-    businessMenu.classList.remove("show");
-    shippingMenu.classList.remove("show");
-    trackingMenu.classList.remove("show");
-});
+// supportTitle.addEventListener('click', () => {
+//     supportMenu.classList.toggle("show");
+//     businessMenu.classList.remove("show");
+//     shippingMenu.classList.remove("show");
+//     trackingMenu.classList.remove("show");
+// });
 
 
