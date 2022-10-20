8951af084b1eb383a855e152018df1ad0c74f4a3
Anuj <anujtanwar89@gmail.com>
2022-10-20 17:15:43 -0400

    :bug:

--- beta-build/main.js
+++ beta-build/main.js
@@ -89,9 +89,9 @@
 function myFunction() {
     var x = document.getElementById("container");
     if (x.style.display === "none") {
-      x.style.display = "flex";
-    } else {
       x.style.display = "none";
+    } else {
+      x.style.display = "flex";
     }
   }
 
